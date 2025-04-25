184dcdb3b0d6824a1c6c67daf4192b1ad0a50a69
unknown <sc465182@gmail.com>
2025-04-25 16:09:13 +0530

    create routes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import "./Navbar.css"
 import logo from "../../assets/logo.webp"
 import cart_icon from "../../assets/cart_icon.png"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const [menu,setMenu]=useState("home");
@@ -12,14 +13,14 @@ const Navbar = () => {
             <p>Shopify</p>
         </div>
         <ul className='nav-menu'>
-            <li onClick={()=>{setMenu("home")}}>Home{menu==="home" ? <hr />:<></>}</li>
-            <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens" ? <hr />:<></>}</li>
-            <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens" ? <hr />:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids" ? <hr />:<></>}</li>
+            <li onClick={()=>{setMenu("home")}}><Link to="/">Home</Link>{menu==="home" ? <hr />:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}><Link to="/mens">Men</Link>{menu==="mens" ? <hr />:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}><Link to="/womens">Women</Link>{menu==="womens" ? <hr />:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}><Link to="/kids">Kids</Link>{menu==="kids" ? <hr />:<></>}</li>
         </ul>
         <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={cart_icon} alt="" height="40px" />
+           <Link to="/login"><button>Login</button></Link> 
+           <Link to="/cart"><img src={cart_icon} alt="" height="40px" /></Link> 
             <div className="nav-cart-count">0</div>
         </div>
     </div>
