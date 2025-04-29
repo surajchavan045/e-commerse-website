f9ccfc8f72db4fed5df41bd1e7138dff79be6a6f
unknown <sc465182@gmail.com>
2025-04-29 17:06:21 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Product from './Pages/Product'
 import Cart from './Pages/Cart'
 import LoginSignup from './Pages/LoginSignup'
 import Footer from './Components/Footer/Footer'
+import men_banner from "./assets/banner.jpg"
+import women_banner from "./assets/women_banner.avif"
+import kids_banner from "./assets/kids_banner.jpg"
 
 function App() {
 
@@ -17,7 +20,7 @@ function App() {
     <Navbar />
     <Routes>
       <Route path='/' element={<Shop/>}/>
-      <Route path='/mens' element={<ShopCategory category="mens"/>}/>
+      <Route path='/mens' element={<ShopCategory banner category="mens"/>}/>
       <Route path='/womens' element={<ShopCategory category="womens"/>}/>
       <Route path='/kids' element={<ShopCategory category="kids"/>}/>
       <Route path='/product' element={<Product/>} />
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import "../Css/ShopCategory.css"
+import { ShopContext } from '../Context/ShopContext'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const {all_product}=useContext(ShopContext)
   return (
-    <div>
-      category
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
     </div>
   )
 }
