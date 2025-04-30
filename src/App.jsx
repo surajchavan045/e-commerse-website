b0bf6d5ae45bd287997c1f97768c51c83e3e53e6
unknown <sc465182@gmail.com>
2025-04-30 14:17:54 +0530

    create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,44 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
-import Shop from './Pages/Shop'
-import ShopCategory from './Pages/ShopCategory'
-import Product from './Pages/Product'
-import Cart from './Pages/Cart'
-import LoginSignup from './Pages/LoginSignup'
-import Footer from './Components/Footer/Footer'
-import men_banner from "./assets/banner.jpg"
-import women_banner from "./assets/women_banner.avif"
-import kids_banner from "./assets/kids_banner.jpg"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
+import men_banner from "./assets/banner.jpg";
+import women_banner from "./assets/women_banner.avif";
+import kids_banner from "./assets/kids_banner.jpg";
 
 function App() {
-
   return (
-   <div>
-    <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Shop/>}/>
-      <Route path='/mens' element={<ShopCategory banner category="mens"/>}/>
-      <Route path='/womens' element={<ShopCategory category="womens"/>}/>
-      <Route path='/kids' element={<ShopCategory category="kids"/>}/>
-      <Route path='/product' element={<Product/>} />
-      <Route path=':productId' element={<Product/>} />
-      <Route path='/cart' element={<Cart/>} />
-      <Route path='/login' element={<LoginSignup/>} />
-    </Routes>
-    <Footer/>
-    </BrowserRouter>
-    
-   </div>
-  )
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kids_banner} category="kid" />}
+          />
+          <Route path="/product" element={<Product />} />
+          <Route path=":productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import "../Css/LoginSignup.css";
 
 const LoginSignup = () => {
   return (
-    <div>
-      login
+    <div className="loginsignup">
+      <div className="liginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+          <button>continue</button>
+        </div>
+        
+        <p className="loginsignup-login">
+          Already have an account ? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
